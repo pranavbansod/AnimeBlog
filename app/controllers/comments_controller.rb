b3d8e91e93033dbb77061351de8a618c9d6986c5
Pranav Bansod <pranavb@thoughtworks.com>
2018-08-14 12:48:02 +0530

    Added create action for comment, Added comments on anime_blog/show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @blog = AnimeBlog.find(params[:anime_blog_id])
+    @comment = @blog.comments.create(comment_params)
+    redirect_to anime_blog_path(@blog)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commented_by, :comment_text)
+  end
 end
