5f039bb77d4ba38b233689c510ba3534adee17a4
Pranav Bansod <pranavb@thoughtworks.com>
2018-08-14 12:20:05 +0530

    Added comment form on show blog page

--- app/models/anime_blog.rb
+++ app/models/anime_blog.rb
@@ -1,2 +1,3 @@
 class AnimeBlog < ApplicationRecord
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :anime_blogs
+  resources :anime_blogs do
+    resources :comments
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
