fe80a81e71960ead72d5857b629070c9bed7dd4f
Pranav Bansod <pranavb@thoughtworks.com>
2018-08-23 11:09:47 +0530

    Added welcome controller test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,7 +3,19 @@ require 'test_helper'
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get welcome_index_url
+    assert_select 'title', "Blog"
     assert_response :success
   end
 
+  test "should have links to change language" do
+    get welcome_index_url
+    assert_select 'a', "Jap"
+    assert_select 'a', "Eng"
+  end
+
+  test "should have links to create blog and view all anime blog" do
+    get welcome_index_url
+    assert_select 'a', "Create New Blog"
+    assert_select 'a', "View All"
+  end
 end
