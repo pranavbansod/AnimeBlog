c85b51577fa54dcd980662c990a0da3a371c5895
Pranav Bansod <pranavb@thoughtworks.com>
2018-08-24 12:49:01 +0530

    Minor fix in anime_blogs_controller_spec

--- spec/controllers/anime_blogs_controller_spec.rb
+++ spec/controllers/anime_blogs_controller_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AnimeBlogsController, type: :controller do
-
-  before(:each) do
-    @anime_blog = AnimeBlog.new(anime_name: 'Another')
-    @anime_blog.save
-  end
-
-
   it "GET index" do
     get :index
     expect(response).to render_template(:index)
+    expect(response).to have_http_status(:ok)
   end
 
   it "POST create" do
     post 'create', params:{anime_blog:{anime_name: 'Another'}}
-    expect(response).to redirect_to(anime_blog_path(@anime_blog.id+1))
+    expect(response).to redirect_to(anime_blog_path(assigns(:blog)))
     expect(assigns(:blog)).to be_an_instance_of(AnimeBlog)
   end
 
